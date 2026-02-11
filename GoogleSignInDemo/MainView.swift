a8f58532ec5fecff07364af312849104574921e7
Kelvin Campos Ricafort <kelvs.r@outlook.com>
2026-02-11 15:23:05 +0800

    - Add the sign in button

--- GoogleSignInDemo/MainView.swift
+++ GoogleSignInDemo/MainView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import GoogleSignIn
+import GoogleSignInSwift
 
 struct MainView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            GoogleSignInButton(action: handleSignInButton).padding()
         }
-        .padding()
+    }
+    
+    func handleSignInButton() {
+        // Find the current window scene.
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else {
+            print("There is no active window scene.")
+            return
+        }
+        
+        // Get the root view controller from the window scene.
+        guard let rootViewController = windowScene.windows.first(where: { $0.isKeyWindow })?.rootViewController else {
+            print("There is no key window or root view controller")
+            return
+        }
+        
+        // Start the sign in process.
+        GIDSignIn.sharedInstance.signIn(withPresenting: rootViewController) { signInResult, error in
+            guard let result = signInResult else {
+                // Inspect error
+                print("Errpr signing in: \(error?.localizedDescription ?? "No error description")")
+                return
+            }
+            // If sign in succeeded, display the app's main content view.
+            print("ID Token: \(result.user.idToken?.tokenString ?? "")")
+        }
     }
 }
 
