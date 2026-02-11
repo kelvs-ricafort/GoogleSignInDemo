913fe1b44219207d28d50d3568269c93ec23ca96
Kelvin Campos Ricafort <kelvs.r@outlook.com>
2026-02-11 15:04:43 +0800

    Initial Commit

---
+++ GoogleSignInDemo/GoogleSignInDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSignInDemoApp.swift
+//  GoogleSignInDemo
+//
+//  Created by Kelvin Ricafort on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSignInDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
